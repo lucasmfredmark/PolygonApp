21e99c50c0541df9384d670a304d3f98dd514b3d
lucasmfredmark <lucas.m.fredmark@gmail.com>
2016-03-30 12:20:42 +0200

    Changed some names and added a foreign key to the buildings table

--- polygondb.sql
+++ polygondb.sql
@@ -1,50 +1,48 @@
-CREATE DATABASE Polygon;
-USE Polygon;
+CREATE DATABASE polygon;
+USE polygon;
 
-CREATE TABLE Building(
-bname varchar(10),
-adress varchar(10),
-parcelnumber int(5),
-size_m2 varchar(3),
-coditionlevel int(1)
+CREATE TABLE buildings (
+    buildingid INT AUTO_INCREMENT PRIMARY KEY,
+    bname varchar(10),
+    address varchar(10),
+    parcelnumber int(5),
+    size_m2 varchar(3),
+    conditionlevel int(1),
+    userid INT
 );
 
-CREATE TABLE Document(
-type varchar(100)
+CREATE TABLE documents (
+    type varchar(100)
 );
 
-CREATE TABLE Service(
-type varchar(100)
+CREATE TABLE services (
+    type varchar(100)
 );
 
-CREATE TABLE Checkup(
-conditionlvl int(1)
+CREATE TABLE checkups (
+    conditionlevel int(1)
 );
 
-CREATE TABLE Users (
-userid INT AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(30),
-userpass VARCHAR(30),
-usertype ENUM('customer','admin'),
-fullname VARCHAR(50),
-email VARCHAR(40)
+CREATE TABLE users (
+    userid INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30),
+    userpass VARCHAR(30),
+    usertype ENUM('customer','admin'),
+    fullname VARCHAR(50),
+    email VARCHAR(40)
 );
 
-CREATE TABLE Municipality(
-mname varchar(30),
-phonenubmer varchar(10)
+CREATE TABLE municipality(
+    mname varchar(30),
+    phonenubmer varchar(10)
 );
 
 CREATE TABLE HousingAssociation(
-hname varchar(30),
-phonenubmer varchar(10)
+    hname varchar(30),
+    phonenubmer varchar(10)
 );
 
 CREATE TABLE PrivatePerson(
-pname varchar(30),
-phonenubmer varchar(10)
+    pname varchar(30),
+    phonenubmer varchar(10)
 );
-
-
-
-
