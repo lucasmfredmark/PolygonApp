c86c188641d7c945ec620e19f55092a154a2c039
HazemSaeid <hazem2670@live.dk>
2016-03-30 12:37:42 +0200

    User, database

---
+++ src/java/serviceLayer/entities/User.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serviceLayer.entities;
+
+import java.util.Enumeration;
+
+/**
+ *
+ * @author HazemSaeid
+ */
+public class User {
+    int id;
+    String uname = "";
+    public enum userType {
+        PRIVATEPERSON, MUNICIPALITY, HOUSINGASSOCIATION,ADMIN;
+    }
+    String fullName = ""; 
+    String email = "";
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
